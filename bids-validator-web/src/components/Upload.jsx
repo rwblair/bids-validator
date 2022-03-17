33aa2412701005fc937422c5dd2becfcea45957e
Ross Blair <rosswilsonblair@gmail.com>
2022-03-17 10:19:26 -0500

    convert upload component to functional to avoid findDOM calls

--- bids-validator-web/src/components/Upload.jsx
+++ bids-validator-web/src/components/Upload.jsx
@@ -3,49 +3,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class Upload extends React.Component {
-  // life cycle events --------------------------------------------------
-
-  componentDidMount() {
-    ReactDOM.findDOMNode(this).setAttribute('webkitdirectory', true)
-    ReactDOM.findDOMNode(this).setAttribute('directory', true)
-    this._setRefs(this.refs)
-  }
-
-  render() {
-    return (
-      <input
-        type="file"
-        className="dirUpload-btn"
-        onClick={this._click.bind(this)}
-        onChange={this._onFileSelect.bind(this)}
-        ref="fileSelect"
-      />
-    )
-  }
-
-  // custom methods -----------------------------------------------------
-
-  _click(e) {
-    ReactDOM.findDOMNode(this.refs.fileSelect).value = null
-    if (this.props.onClick) {
-      this.props.onClick(e)
+const Upload = (props) => {
+  const fileSelect  = React.useRef(null);
+  React.useEffect(() => {
+    fileSelect.current.setAttribute('webkitdirectory', true)
+    fileSelect.current.setAttribute('directory', true)
+  })
+  const _click = (e) => {
+    if (props.onClick) {
+      props.onClick(e)
     }
   }
 
-  _onFileSelect(e) {
+  const _onFileSelect = (e) => {
     if (e.target && e.target.files.length > 0) {
       let files = e.target.files
       let results = { list: files }
-      this.props.onChange(results)
+      props.onChange(results)
     }
   }
 
-  _setRefs(refs) {
-    if (this.props.setRefs) {
-      this.props.setRefs(refs)
-    }
-  }
+  return (
+      <input
+        type="file"
+        className="dirUpload-btn"
+        onClick={_click}
+        onChange={_onFileSelect}
+        ref={fileSelect}
+      />
+  )
 }
 
 export default Upload
