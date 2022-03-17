3feb32dd8ab2cd1d07d94b5659eaf6c3d3cd80d1
Ross Blair <rosswilsonblair@gmail.com>
2022-03-17 12:48:06 -0500

    remove jshint dependency and try my fork of nifti-js to fix esbuild issues

--- bids-validator/utils/json.js
+++ bids-validator/utils/json.js
@@ -1,5 +1,4 @@
 import Issue from './issues'
-import { JSHINT } from 'jshint'
 
 /**
  * Similar to native JSON.parse but returns a promise and
@@ -15,8 +14,12 @@ function parse(file, contents) {
       err = exception
     } finally {
       if (err) {
-        jshint(file, contents, function(issues) {
-          resolve({ issues, parsed: null })
+        resolve({
+          issues: [new Issue({
+            code: 27,
+            file: file,
+            reason: err.toString()
+          })],
         })
       } else {
         resolve({ issues: [], parsed: jsObj })
@@ -25,37 +28,6 @@ function parse(file, contents) {
   })
 }
 
-/**
- * JSHint
- *
- * Checks known invalid JSON file
- * content in order to produce a
- * verbose error message.
- */
-function jshint(file, contents, callback) {
-  var issues = []
-  if (!JSHINT(contents)) {
-    var out = JSHINT.data()
-    for (var i = 0; out.errors.length > i; ++i) {
-      var error = out.errors[i]
-      if (error) {
-        issues.push(
-          new Issue({
-            code: 27,
-            file: file,
-            line: error.line ? error.line : null,
-            character: error.character ? error.character : null,
-            reason: error.reason ? error.reason : null,
-            evidence: error.evidence ? error.evidence : null,
-          }),
-        )
-      }
-    }
-  }
-  callback(issues)
-}
-
 export default {
-  parse,
-  jshint,
+  parse
 }
